cec1697340eb13638dcef282d9c0f82ce4a4bece
Ceyda Ulubas <ceyda.ulubas@gmail.com>
2021-04-12 21:34:58 +0300

    added new projects-5 and 6

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,6 +7,8 @@ import PetAdopt from "../images/WebProjectScreen/petadopt1.png"
 import CodeChat from "../images/WebProjectScreen/codeChat-screenhot.png"
 import HalloweenSnake from "../images/WebProjectScreen/HalloweenSnake-screenshot.png"
 import TodoList from "../images/WebProjectScreen/todolist-angular-pagepic.png"
+import EasyInsurance from "../images/WebProjectScreen/EasyInsurance.png"
+import GiphyApi from "../images/WebProjectScreen/Gipgy.jpg"
 
 import BoxP from "../layout/BoxP";
 
@@ -130,6 +132,71 @@ const projects = () => {
             <a
               className="btn-primary pt-1 pb-1 pl-2 pr-2"
               href="https://github.com/ceydaulubas/ToDoList-Angular-CLI-FakeBackEnd"
+              target="_blank"
+              rel=" noopener noreferrer"
+            >
+              Github
+          </a>
+          </Card.Footer>
+        </Card>
+
+         {/* PROJECT 5 */}
+         <Card className="projectL" style={{  borderRadius: "25px" }} >
+          <Card.Img variant="top" src={EasyInsurance} />
+          <Card.Body>
+            <Card.Title>Easy Insurance</Card.Title>
+            <Card.Text  >
+            Easy Insurance is an application that can see all invoices and add new invoices. 
+            Thanks to the filter feature people can filter and see invoices that they want to see directly.
+        </Card.Text>
+            <Card.Text >
+              <b> Stack: </b> React, Webpack, Babel 
+        </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+          <a
+              className="btn-warning pt-1 pb-1 pl-2 pr-2 mr-4"
+              href="https://easy-insurance.netlify.app/"
+              target="_blank"
+              rel=" noopener noreferrer"
+            >
+              Website
+            </a>
+            <a
+              className="btn-primary pt-1 pb-1 pl-2 pr-2"
+              href="https://github.com/ceydaulubas/react-webpack-babel"
+              target="_blank"
+              rel=" noopener noreferrer"
+            >
+              Github
+          </a>
+          </Card.Footer>
+        </Card>
+
+        {/* PROJECT 6 */}
+         <Card className="projectR" style={{ borderRadius: "25px"}}>
+          <Card.Img variant="top" src={GiphyApi} />
+          <Card.Body>
+            <Card.Title>Giphy Api</Card.Title>
+            <Card.Text>
+            This app shows all gifs when you write a word that you want to see as a gif.
+        </Card.Text>
+            <Card.Text >
+              <b> Stack: </b> React Js
+        </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+          <a
+              className="btn-warning pt-1 pb-1 pl-2 pr-2 mr-4"
+              href="https://giphy-api-react.netlify.app/"
+              target="_blank"
+              rel=" noopener noreferrer"
+            >
+              Website
+            </a>
+            <a
+              className="btn-primary pt-1 pb-1 pl-2 pr-2"
+              href="https://github.com/codebrokerad/halloweenSnake"
               target="_blank"
               rel=" noopener noreferrer"
             >
