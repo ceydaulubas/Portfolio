19c9e69f82d1c5c29071d2a2ef73114c153c7ebf
Ceyda Ulubas <ceyda.ulubas@gmail.com>
2021-04-19 22:04:17 +0300

    added weather forecast project info

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "../styles/Projects.scss";
-// import YouTubeVideo from "../thirdParty/YouTube";
+
 import Card from "react-bootstrap/Card";
 // import data from "./data/data.json";
 import PetAdopt from "../images/WebProjectScreen/petadopt1.png"
@@ -9,6 +9,7 @@ import HalloweenSnake from "../images/WebProjectScreen/HalloweenSnake-screenshot
 import TodoList from "../images/WebProjectScreen/todolist-angular-pagepic.png"
 import EasyInsurance from "../images/WebProjectScreen/EasyInsurance.png"
 import GiphyApi from "../images/WebProjectScreen/Gipgy.jpg"
+import WeatherForeCast from "../images/WebProjectScreen/weather-api-mainscreen.jpg"
 
 import BoxP from "../layout/BoxP";
 
@@ -50,7 +51,7 @@ const projects = () => {
         </Card>
 
         {/* PROJECT 2 */}
-        <Card className="projectR" style={{ borderRadius: "25px"}} >
+        <Card className="projectR" style={{ borderRadius: "25px" }} >
           <Card.Img variant="top" src={CodeChat} />
           <Card.Body>
             <Card.Title>CodeChat</Card.Title>
@@ -83,7 +84,55 @@ const projects = () => {
         </Card>
 
         {/* PROJECT 3 */}
-        <Card className="projectR" style={{ borderRadius: "25px"}}>
+        <Card className="projectL" style={{ borderRadius: "25px" }}>
+          <Card.Img variant="top" src={WeatherForeCast} className="projectImage" />
+          <Card.Body>
+            <Card.Title >Weather Forecast</Card.Title>
+            <Card.Text >
+            Weather forecast website gives information about weather like current weather, coming days weather, min-max temperatures, etc.
+        </Card.Text>
+            <Card.Text style={{ marginBottom: '1%' }}>
+              <b> Stack: </b> React Js, Axios, CSS
+        </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <a
+              className="btn-primary pt-1 pb-1 pl-2 pr-2"
+              href="https://github.com/ceydaulubas/reactjs-weather-api"
+              target="_blank"
+              rel=" noopener noreferrer"
+            >
+              Github
+          </a>
+          </Card.Footer>
+        </Card>
+
+        {/* PROJECT 4 */}
+        <Card className="projectR" style={{ borderRadius: "25px" }} >
+          <Card.Img variant="top" src={TodoList}/>
+          <Card.Body>
+            <Card.Title>Todo List</Card.Title>
+            <Card.Text  >
+              The first project that I created using the Angular framework and mocked JSON file.
+        </Card.Text>
+            <Card.Text style={{ marginBottom: '1%' }} >
+              <b> Stack: </b> Typecript, SCSS, Angular CLI
+        </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <a
+              className="btn-primary pt-1 pb-1 pl-2 pr-2"
+              href="https://github.com/ceydaulubas/ToDoList-Angular-CLI-FakeBackEnd"
+              target="_blank"
+              rel=" noopener noreferrer"
+            >
+              Github
+          </a>
+          </Card.Footer>
+        </Card>
+
+        {/* PROJECT 5 */}
+        <Card className="projectR" style={{ borderRadius: "25px" }}>
           <Card.Img variant="top" src={HalloweenSnake} />
           <Card.Body>
             <Card.Title>HalloweenSnake</Card.Title>
@@ -91,7 +140,7 @@ const projects = () => {
               First group project in Ironhack.
               Halloween Snake is a modified version of the Nostalgic snake game with the Halloween theme.
         </Card.Text>
-            <Card.Text >
+            <Card.Text style={{ marginBottom: '1%' }} >
               <b> Stack: </b> Javascript, HTML, CSS
         </Card.Text>
           </Card.Body>
@@ -115,45 +164,23 @@ const projects = () => {
           </Card.Footer>
         </Card>
 
-        {/* PROJECT 4 */}
-        <Card className="projectL" style={{  borderRadius: "25px" }} >
-          <Card.Img variant="top" src={TodoList} />
-          <Card.Body>
-            <Card.Title>Todo List</Card.Title>
-            <Card.Text  >
-              The first project that I created using the Angular framework and mocked JSON file.
-        </Card.Text>
-            <Card.Text >
-              <b> Stack: </b> Typecript, SCSS, Angular CLI
-        </Card.Text>
-          </Card.Body>
-          <Card.Footer>
-            <a
-              className="btn-primary pt-1 pb-1 pl-2 pr-2"
-              href="https://github.com/ceydaulubas/ToDoList-Angular-CLI-FakeBackEnd"
-              target="_blank"
-              rel=" noopener noreferrer"
-            >
-              Github
-          </a>
-          </Card.Footer>
-        </Card>
 
-         {/* PROJECT 5 */}
-         <Card className="projectL" style={{  borderRadius: "25px" }} >
+
+        {/* PROJECT 6 */}
+        <Card className="projectL" style={{ borderRadius: "25px" }} >
           <Card.Img variant="top" src={EasyInsurance} />
           <Card.Body>
             <Card.Title>Easy Insurance</Card.Title>
             <Card.Text  >
-            Easy Insurance is an application that can see all invoices and add new invoices. 
-            Thanks to the filter feature people can filter and see invoices that they want to see directly.
+              Easy Insurance is an application that can see all invoices and add new invoices.
+              Thanks to the filter feature people can filter and see invoices that they want to see directly.
         </Card.Text>
             <Card.Text >
-              <b> Stack: </b> React, Webpack, Babel 
+              <b> Stack: </b> React, Webpack, Babel
         </Card.Text>
           </Card.Body>
           <Card.Footer>
-          {/* <a
+            {/* <a
               className="btn-warning pt-1 pb-1 pl-2 pr-2 mr-4"
               href="https://easy-insurance.netlify.app/"
               target="_blank"
@@ -172,20 +199,20 @@ const projects = () => {
           </Card.Footer>
         </Card>
 
-        {/* PROJECT 6 */}
-         <Card className="projectR" style={{ borderRadius: "25px"}}>
+        {/* PROJECT 7 */}
+        <Card className="projectL" style={{ borderRadius: "25px" }}>
           <Card.Img variant="top" src={GiphyApi} />
           <Card.Body>
             <Card.Title>Giphy Api</Card.Title>
             <Card.Text>
-            This app shows all gifs when you write a word that you want to see as a gif.
+              This app shows all gifs when you write a word that you want to see as a gif.
         </Card.Text>
             <Card.Text >
               <b> Stack: </b> React Js, Axios, CSS
         </Card.Text>
           </Card.Body>
           <Card.Footer>
-          <a
+            <a
               className="btn-warning pt-1 pb-1 pl-2 pr-2 mr-4"
               href="https://giphy-api-react.netlify.app/"
               target="_blank"
